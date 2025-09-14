23e9d5f2e512aadd5685980955ee396210f95fc9
MatheusZamo <19matheushenrique03@gmail.com>
2025-09-13 22:59:43 -0300

    Add root path

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,29 @@ import { createRoot } from "react-dom/client"
 import { App } from "./app.jsx"
 import "./index.css"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router"
 
 const rootElement = document.querySelector('[data-js="root"]')
 const root = createRoot(rootElement)
 const queryClient = new QueryClient()
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="\">
+      <Route index element={<App />} />
+    </Route>,
+  ),
+)
+
 root.render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
